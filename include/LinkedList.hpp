ca5fcfbdc93e7eaa2902af7e553727e930475a99
J_Fellouse <jules.fellouse@gmail.com>
2024-05-10 02:00:53 +0200

    added linkedlist with iterator behaviour

---
+++ include/LinkedList.hpp
@@ -0,0 +1,119 @@
+#pragma once
+
+#include "stddef.h"
+
+//jds = jules data structures
+namespace jds
+{
+    template<typename T>
+    class LinkedList
+    {
+        private:
+            //Node of the linked list
+            struct Node
+            {
+                T data;
+                Node* nextNode;
+
+                Node(T data, Node* nextNode)
+                    : data(data), nextNode(nextNode) {}
+            };
+
+            class Iterator;
+
+            Node* _headNode = nullptr;
+            Node* _lastNode = nullptr;
+            size_t _size = 0;
+        public:
+            //Delete all dynamically created LinkedListNode
+            ~LinkedList();
+
+            //Add an element at the end of the linked list
+            void push_back(const T data);
+
+            //Add the element at the top of the linked list
+            void push_front(const T data);
+
+            //Get the actual number of nodes in the linked list
+            size_t size() const {return _size; }
+            
+            //Standard STL method used for iteration using for loop
+            Iterator begin() const { return Iterator(_headNode); }
+            
+            //Standard STL method used for iteration using for loop
+            Iterator end() const { return Iterator(nullptr); }
+    };
+
+    //Internal Iterator class
+    template <typename T>
+    class LinkedList<T>::Iterator
+    {
+        private:
+            Node* current;
+        public:
+            Iterator(Node* node = nullptr) : current(node) {}
+
+            //Iterating behavior
+            Iterator& operator++()
+            {
+                current = current->nextNode;
+                return *this;
+            }
+
+            //Used to access the data by reference
+            T& operator*() { return current->data; }
+
+            //Used to access the data via pointer
+            T* operator->() { return &current->data; }
+
+            //Inequality operator generally used as a condition to stop the loop
+            bool operator!=(const Iterator& other) const { return current != other.current; }
+
+            //Equality operator generally used as a condition to stop the loop
+            bool operator==(const Iterator& other) const { return current == other.current; }
+    };
+
+    template <typename T>
+    LinkedList<T>::~LinkedList()
+    {
+        struct Node* current = _headNode;
+
+        while (current != nullptr)
+        {
+            struct Node* next = current->nextNode;
+            delete current;
+            current = next;
+        }
+    }
+
+    template <typename T>
+    void LinkedList<T>::push_back(const T data)
+    {        
+        Node* newNode = new Node(data, nullptr);
+        _size++;
+
+        if(_headNode == nullptr)
+        {
+            _headNode = newNode;
+            _lastNode = _headNode;
+            return;
+        }
+
+        _lastNode->nextNode = newNode;
+        _lastNode = newNode;
+    }
+
+    template <typename T>
+    void LinkedList<T>::push_front(const T data)
+    {
+        Node* newNode = new Node(data, _headNode);
+        _size++;
+
+        _headNode = newNode;
+
+        if(_lastNode == nullptr)
+        {
+            _lastNode = _headNode;
+        }
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include "LinkedList.hpp"
 
 int main()
 {
-    std::cout << "Hello world !" << std::endl;
+    jds::LinkedList<int> myLinkedList;
+    myLinkedList.push_back(888);
+    myLinkedList.push_back(777);
+    myLinkedList.push_back(666);
+
+    for(int &i : myLinkedList)
+    {
+        i = 3;
+    }
+
+    for(int i : myLinkedList)
+    {
+        std::cout << i << std::endl;
+    }
 }
